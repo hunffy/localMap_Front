8180b1df97334a39e5f2511f366dc15dd517b19a
ksh <sehun910124@naver.com>
2023-08-23 10:43:39 +0900

    숙제제출

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,12 +1,12 @@
-export const BaseUrl = 'http://127.0.0.1:8000/api'
+export const BaseUrl = "http://127.0.0.1:8000/api";
 
 export type Notice = {
-  id: string
-}
+  id: string;
+};
 
 export type User = {
-  id: string
-  name: string
-  email: string
-  password: string
-}
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
