6f8fcb660430686daa92d43e512cb1430bdb98da
henry <henry@yescnc.co.kr>
2023-08-25 22:11:20 +0900

    fix component

--- src/components/mainSearch.tsx
+++ src/components/mainSearch.tsx
@@ -1,20 +1,20 @@
-import locationIcon from "../assets/images/ic_header_search.svg";
-import searchIcon from "../assets/images/ic_main_search.svg";
-import { ChangeEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import locationIcon from '../assets/images/ic_header_search.svg'
+import searchIcon from '../assets/images/ic_main_search.svg'
+import { ChangeEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const MainSearch = () => {
-  const [searchValue, setSearch] = useState("");
+  const [searchValue, setSearch] = useState('')
   const saveSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
-  };
+    setSearch(event.target.value)
+  }
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const goSearchResult = () => {
-    navigate("/searchResult" + `/${searchValue}`);
-  };
+    navigate('/searchResult' + `/${searchValue}`)
+  }
   return (
-    <div className="searchWrapper">
+    <div className="mainSearchWrapper">
       <div className="search">
         <img src={locationIcon} />
         <input type="text" value={searchValue} onChange={saveSearch} />
@@ -24,7 +24,7 @@ const MainSearch = () => {
         <p>검색</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MainSearch;
+export default MainSearch
